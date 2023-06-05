b0112080dab904582beceade6b684d881504c47c
Kay <info@beckmann-md.de>
2023-06-05 19:43:50 +0200

    designed game-table

--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Route, Router } from "@angular/router";
 
 @Component({
-  selector: 'app-start-screen',
-  templateUrl: './start-screen.component.html',
-  styleUrls: ['./start-screen.component.scss']
+  selector: "app-start-screen",
+  templateUrl: "./start-screen.component.html",
+  styleUrls: ["./start-screen.component.scss"]
 })
 export class StartScreenComponent {
+  constructor(private router: Router) {}
 
+  newGame() {
+    this.router.navigateByUrl(`/game`);
+  }
 }
