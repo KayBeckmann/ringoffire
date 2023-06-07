c21e24523950317ec8798ed7e51b1a234ff7af4d
Kay <info@beckmann-md.de>
2023-06-07 17:29:41 +0200

    Added component add-user.

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Game } from "src/models/game";
+import { MatDialog } from "@angular/material/dialog";
+import { DialogAddPlayerComponent } from "../dialog-add-player/dialog-add-player.component";
 
 @Component({
   selector: "app-game",
@@ -11,7 +13,7 @@ export class GameComponent implements OnInit {
   game: Game = new Game();
   currentCard: string = ``;
 
-  constructor() {}
+  constructor(public dialog: MatDialog) {}
 
   ngOnInit(): void {
     this.newGame();
@@ -35,5 +37,13 @@ export class GameComponent implements OnInit {
 
   dropCard() {
     this.pickCardAnimation = false;
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent);
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log("The dialog was closed");
+    });
   }
 }
